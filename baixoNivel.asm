10a2033f3a80148b4cb25a1f156669b5475ffa4b
Eduarda <eduarda.santos.patricio@gmail.com>
2024-04-21 21:26:33 -0300

    Comentários adicionados :)

--- baixoNivel.asm
+++ baixoNivel.asm
@@ -1,116 +1,106 @@
-.macro prtStr(%string)				#macro para printar strings
-	addi 	$sp, $sp, -8			#abrindo espa�o na pilha para dois registradores 
-	sw 	$v0, 0($sp)			#colocando o que esta no registrador v0 no inicio da pilha
-	sw 	$a0, 4($sp)			#colocando o que esta no registrador a0 no fim da pilha
-	
-	la 	$a0, %string			#carregando o endere�o da variavel string para  a0
-	li 	$v0, 4 				#carrega a instru��o 4 para v0
-	syscall					#chama o SO para executar a opera��o 4 (q ta em v0). 4 imprime strings
-	
-	lw 	$a0, 4($sp)			#carregando valor que esta no fim da pilha de volta ao a0 
-	lw 	$v0, 0($sp)			#carregando o valor que esta no inicio da pilha de volta ao v0 
-	addi 	$sp, $sp, 8			#apagando o espa�o aberto na pilha
+.macro prtStr(%string)	    		# Macro para printar strings
+	addi 	$sp, $sp, -8		# Abre dois espacos na pilha
+	sw 	$v0, 0($sp)		# Guarda o valor que esta no registrador $v0 no inicio da pilha
+	sw 	$a0, 4($sp)		# Guarda o valor que esta no registrador $a0 no final da pilha
+	la 	$a0, %string		# Carrega o endereco da string em $a0
+	li 	$v0, 4 			# Carrega a instrucao 4 (print_string) em $v0
+	syscall				# Chama o sistema para executar a instrucao
+	lw 	$a0, 4($sp)		# Recupera o valor que esta no final da pilha de volta ao $a0 
+	lw 	$v0, 0($sp)		# Recupera o valor que esta no inicio da pilha de volta ao $v0 
+	addi 	$sp, $sp, 8		# Apaga o espaco aberto na pilha
 .end_macro 
 
-.macro leInt(%int)				#macro para ler um valor inteiro
-	addi	$sp, $sp, -4			#abrindo espa�o na pilha para um int
-	sw	$v0, 0($sp)			#colocando o que esta no registrador v0 no inicio da pilha
-	
-	li	$v0, 5				#carrega a instru��o 5 para v0
-	syscall					#chama o SO para executar a opera��o 5 (q ta em v0). 5 l� inteiros
-	move 	%int, $v0			#move o valor de v0 para o destino %int
-	
-	lw 	$v0, 0($sp)			#carregando o valor que esta no inicio da pilha de volta ao v0 
-	addi 	$sp, $sp, 4			#apaga o espa�o aberto na pilha
+.macro leInt(%int)			# Macro para ler um valor inteiro
+	addi	$sp, $sp, -4		# Abre espaco na pilha para um int
+	sw	$v0, 0($sp)		# Coloca o que esta no registrador $v0 no inicio da pilha
+	li	$v0, 5			# Carrega a instrucao 5 (read_int) em $v0
+	syscall				# Chama o sistema para executar a instrucao
+	move 	%int, $v0		# Move o valor de $v0 para o destino %int
+	lw 	$v0, 0($sp)		# Recupera o valor que esta no inicio da pilha de volta ao $v0 
+	addi 	$sp, $sp, 4		# Apaga o espaco aberto na pilha
 .end_macro
 
-.macro prtInt(%inteiro)
-	addi	$sp, $sp, -8
-	sw	$v0, 0($sp)	
-	sw	$a0, 4($sp)	
-	
-	lw	$a0, %inteiro
-	li	$v0, 1
-	syscall
-	
-	lw	$a0, 4($sp)	
-	lw	$v0, 0($sp)	
-	addi	$sp, $sp, 8
+.macro prtInt(%inteiro)			# Macro para printar um valor inteiro			
+	addi	$sp, $sp, -8		# Abre dois espacos na pilha
+	sw	$v0, 0($sp)		# Guarda o valor que esta no registrador $v0 no inicio da pilha
+	sw	$a0, 4($sp)		# Guarda o valor que esta no registrador $a0 no final da pilha
+	lw	$a0, %inteiro		# Carrega o endereco do inteiro em $a0
+	li	$v0, 1			# Carrega a instrucao 1 (print_int) em $v0
+	syscall				# Chama o sistema para executar a instrucao
+	lw	$a0, 4($sp)		# Recupera o valor que esta no final da pilha de volta ao $a0 
+	lw	$v0, 0($sp)		# Recupera o valor que esta no inicio da pilha de volta ao $v0
+	addi	$sp, $sp, 8		# Apaga o espaco aberto na pilha
 .end_macro
 		
-.macro callRaiz()
-	addi	$sp, $sp, -12
-	sw	$a0, 8($sp)
-	sw	$a1, 4($sp)	
-	sw	$ra, 0($sp)
-		
-	jal	raiz	
-			
-	lw	$ra, 0($sp)	
-	lw	$a1, 4($sp)
-	lw	$a0, 8($sp)
-	addi	$sp, $sp, 12
+.macro callRaiz()			# Macro para chamar a função raiz
+	addi	$sp, $sp, -12		# Abre tres espacos na pilha
+	sw	$a0, 8($sp)		# Guarda o valor que esta no registrador $a0 (x) no final da pilha
+	sw	$a1, 4($sp)		# # Guarda o valor que esta no registrador $a1 (i) no meio da pilha
+	sw	$ra, 0($sp)		# Guarda o valor que esta no registrador $r0 no inicio da pilha		
+	jal	raiz			# Vai para raiz
+	lw	$ra, 0($sp)		# Recupera o valor que esta no inicio da pilha de volta ao $ra
+	lw	$a1, 4($sp)		# Recupera o valor que esta no meio da pilha de volta ao $a1
+	lw	$a0, 8($sp)		# Recupera o valor que esta no final da pilha de volta ao $a0
+	addi	$sp, $sp, 12		# Apaga o espaco aberto na pilha
+.end_macro 
+
+.macro resultado()			# Macro para printar o resultado
+	sw	$v0, result		# Salva em result o valor lido em $v0
+	prtStr(strRaizI)		# Print("sqrt("
+	prtInt(x)			# Print("sqrt(" + x"
+	prtStr(strVirg)			# Print("sqrt(" + x + ", "
+	prtInt(i)			# Print("sqrt(" + x + ", " + i
+	prtStr(strRaizF)		# Print("sqrt(" + x + ", " + i + ") = "
+	prtInt(result)			# Print("sqrt(" + x + ", " + i + ") = " + result);
+.end_macro 
+
+.macro exit()				# Macro para sair do programa
+	li	$v0, 10			# Carrega a instrucao 10 (exit) em $v0
+ 	syscall				# Chama o sistema para executar a instrucao
 .end_macro
-
-.macro exit()
-	li	$v0, 10
- 	syscall
-.end_macro 
-
-.macro resultado()
-	sw	$v0, result	
-	prtStr(strRaizI)			#println("sqrt("
-	prtInt(x)				#println("sqrt("+x
-	prtStr(strVirg)				#println("sqrt("+x+","
-	prtInt(i)				#println("sqrt("+x+","+i
-	prtStr(strRaizF)			#println("sqrt("+x+","+i+") = "
-	prtInt(result)				#println("sqrt("+x+","+i+") = " + a);
-.end_macro 
 
 .data
 x: .space 4 
 i: .space 4
 result: .space 4
 strProgm: .asciiz "\nPrograma de Raiz - Newton-Raphson\nDesenvolvedoras: Eduarda Patricio, Giovanna Castro, Naiumy dos Reis e Yasmin Aguirre"
-strDigite: .asciiz "\nDigite os par�metros x e i para calcular sqrt_nr(x, i) ou -1 para abortar a execu��o!"
-strPrmX: .asciiz "\nDigite o par�metro x\n"
-strPrmI: .asciiz "\nDigite o par�metro i\n"
-strRaizI: .asciiz "\n sqrt("
+strDigite: .asciiz "\n\nDigite os parametros x e i para calcular sqrt_nr(x, i) ou -1 para abortar a execucao\nDigite o parametro x: "
+strPrmI: .asciiz "Digite o parametro i: "
+strRaizI: .asciiz "sqrt("
 strVirg: .asciiz ", "
 strRaizF: .asciiz ") = "
 
 .text
 	.globl main
 main:
-	prtStr(strProgm) 			#usa o macro prtStr para printar a string de strProgm
+	prtStr(strProgm) 		# Usa o macro prtStr para printar a string de strProgm
 loop:
-	prtStr(strDigite)			#usa o macro prtStr para printar a string de strDigite
-	prtStr(strPrmX)				#usa o macro prtStr para printar a string de strPrmX
-	leInt($a0)				#usa o macro leInt para ler um valor inteiro
-	sw	$a0, x				#armazena em x o numero lido em a0
-	blt $a0, $zero, fim
-	prtStr(strPrmI)				#usa o macro prtStr para printar a string de strPrmI
-	leInt($a1)				#usa o macro leInt para ler um valor inteiro
-	sw	$a1, i				#armazena em x o numero lido em a1
-	blt $a1, $zero, fim
-	callRaiz()				#Chama o macro que chama a função
-	resultado()
-	jal loop
+	prtStr(strDigite)		# Usa o macro prtStr para printar a string de strDigite
+	leInt($a0)			# Usa o macro leInt para ler o valor inteiro de $a0
+	sw	$a0, x			# Salva em x o valor lido em $a0
+	blt $a0, $zero, fim		# Verifica se a entrada é negativa (x < 0)
+	prtStr(strPrmI)			# Usa o macro prtStr para printar a string de strPrmI
+	leInt($a1)			# Usa o macro leInt para ler o valor inteiro de $a1
+	sw	$a1, i			# Salva em i o valor lido em $a1
+	blt $a1, $zero, fim		# Verifica se a entrada é negativa (i < 0)
+	callRaiz()			# Chama o macro que faz a função de raiz quadrada
+	resultado()			# Chama o macro que imprime o resultado
+	jal loop			# Repete o programa enquanto a entrada for positiva
 fim:
-	exit()					#}
+	exit()				# Chama o macro que encerra o programa
 
 raiz:				
-	lw	$a1, 4($sp)			#i
-	lw	$a0, 8($sp)			#x
-	bgt	$a1, 0, recurs			#if(i > 0) 
-	li	$v0, 1				#if(i == 0){result = 1;}
-	jr	$ra				#Volta
+	lw	$a1, 4($sp)		# Carrega o valor de $a1 em i
+	lw	$a0, 8($sp)		# Carrega o valor de $a0 em x
+	bgt	$a1, $zero, recurs	# Se (i > 0), vai para "recurs"
+	li	$v0, 1			# Define o resultado como 1 se (i == 0)
+	jr	$ra			# Volta para o macro callRaiz()
 recurs:	
-	addiu	$a1, $a1, -1			#i-1
-	callRaiz()	 			#Chama de novo
-	addiu	$a1, $a1, 1			#Volta o i
-	div	$t0, $a0, $v0			#x/sqrt_nr(x, i-1)
-	add	$t1, $t0, $v0			#sqrt_nr(x, i-1) + (x/sqrt_nr(x, i-1)
-	li	$t2, 2				#2
-	div	$v0, $t1, $t2			#(sqrt_nr(x, i-1) + (x/sqrt_nr(x, i-1)))/2;
-	jr	$ra				#Volta para raiz
+	addiu	$a1, $a1, -1		# Decrementa i (i--)
+	callRaiz()	 		# Chama o macro que faz a função de raiz quadrada de novo
+	addiu	$a1, $a1, 1		# Restaura o valor de i (i++)
+	div	$t0, $a0, $v0		# x / sqrt_nr(x, i - 1)
+	add	$t1, $t0, $v0		# sqrt_nr(x, i - 1) + (x / sqrt_nr(x, i - 1)
+	li	$t2, 2			# Carrega o imediato 2 em $t2
+	div	$v0, $t1, $t2		# (sqrt_nr(x, i - 1) + (x / sqrt_nr(x, i - 1))) / 2
+	jr	$ra			# Volta para raiz
